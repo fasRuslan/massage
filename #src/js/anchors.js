19bd555aa80bb68f8fdb1b9fc405bb692fe5242e
fasRuslan <faskhutdinoff.rus@yandex.ru>
2020-08-15 22:42:29 +0300

    саше обновлен

--- #src/js/anchors.js
+++ #src/js/anchors.js
@@ -1,30 +1,7 @@
-// $(function () {
-// 	$('a[data-target^=anchor]').bind('click.smoothscroll', function () {
-// 		let target = $(this).attr('href'),
-// 			bl_top = $(target).offset().top;
-// 		console.log(bl_top);
-// 		$('body,  html').animate({
-// 			scrollTop: bl_top
-// 		}, 700);
-// 		return false;
-// 	})
-// });
-// $(function () {
-// 	$('a[data-target^=anchor]').click(function () {
-// 		var _href = $(this).attr("href");
-// 		$("html").animate({ scrollTop: $(_href).offset().top + "px" });
-// 		console.log(_href);
-// 		console.log($(_href).offset().top);
-// 		return false;
-// 	});
-// });
-// $('a[data-target^=anchor]').on("click", function (e) {
-// 	e.preventDefault();
-// 	var anchor = $(this);
-// 	$('html, body').stop().animate({
-// 		scrollTop: ('700')
-// 	});
-// 	console.log(_href);
-// 	console.log($(_href).offset().top);
-// });
-// Значение от начала страницы а мне нужно чтобы он брал отсчет от начала блока на котором я нахожусь
+$('a[data-target^=anchor]').click(function () {
+	var el = $(this).attr('href');
+	$('html, body').animate({
+		scrollTop: $(el).offset().top
+	}, 1000);
+	return false;
+});
--- #src/js/script.js
+++ #src/js/script.js
@@ -9,4 +9,29 @@ window.addEventListener('DOMContentLoaded', () => {
 	@@include('anchors.js')
 	@@include('slick.js')
 	@@include('slick__rewiews.js')
+	@@include('slick__rewiews.js')
+	@@include('_services__card_link.js')
+	console.log(1);
+
+	let servicesCardLink = document.querySelectorAll('.js__services-card');//popup-link
+	let formServicesTitle = document.querySelector('.form-services__title')//Children[1] form title
+	let servicesCard = document.querySelector('#services__popup-services');//
+	if (servicesCardLink.length > 0) {
+		for (let index = 0; index < servicesCardLink.length; index++) {
+			const linkBtn = servicesCardLink[index];
+			linkBtn.addEventListener('click', changeTitle)
+		}
+	}
+
+	function changeTitle() {
+		let title = this.parentElement.children[1].innerHTML;//название массажа в секции
+		console.log(title);
+		let formTitle = document.querySelector('.js__form-services__title');//название массажа в форме
+		console.log(formTitle);
+		let formSubject = document.querySelector('.js__form-services__subject');//скрытый заголовок который будет отправляться с формой
+		formTitle.innerHTML = title;
+		formSubject.value = title;
+		console.log(formSubject.value);
+	}
+
 });
